b22e93a46f76f7954000d9d912c846da0a980898
Face <69168154+face-hh@users.noreply.github.com>
2024-07-26 16:58:07 +0300

    swap email to discord

--- src/routes/Auth.svelte
+++ src/routes/Auth.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import * as AlertDialog from '@/components/ui/alert-dialog';
 	import { Button } from '@/components/ui/button';
-	import { Input } from '@/components/ui/input';
 	import Label from '@/components/ui/label/label.svelte';
 	import { supabase } from '@/supabase';
+	import { mode } from 'mode-watcher';
 
 	let loading = false;
-	let email = '';
 
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const { error } = await supabase.auth.signInWithOtp({ email });
+			const { error } = await supabase.auth.signInWithOAuth({
+				provider: 'discord'
+			});
 
 			if (error) throw error;
 		} catch (error) {
@@ -31,34 +32,31 @@
 			<Label class="select-none text-8xl">Lyntr.</Label>
 		</div>
 
-		<Label class="text-3xl">Please input your email below.</Label>
+		<Label class="text-3xl">Please log in to begin.</Label>
 
-        <div class="flex w-full items-center space-x-2">
-		<Input
-			type="email"
-			placeholder="johndoe@gmail.com"
-			class="border-primary focus-visible:ring-0 focus-visible:ring-offset-0"
-            bind:value={email}
-		/>
-
-		<AlertDialog.Root>
-			<AlertDialog.Trigger asChild let:builder>
-				<Button builders={[builder]} on:click={handleLogin} disabled={loading}
-					>{loading ? 'Loading' : 'Send magic link'}</Button
-				>
-			</AlertDialog.Trigger>
-			<AlertDialog.Content>
-				<AlertDialog.Header>
-					<AlertDialog.Title>Check your email!</AlertDialog.Title>
-					<AlertDialog.Description>
-						A one-time-password have been sent to your email.
-					</AlertDialog.Description>
-				</AlertDialog.Header>
-				<AlertDialog.Footer>
-					<AlertDialog.Action>Continue</AlertDialog.Action>
-				</AlertDialog.Footer>
-			</AlertDialog.Content>
-		</AlertDialog.Root>
-    </div>
+		<div class="flex w-full items-center justify-center space-x-2">
+			<AlertDialog.Root>
+				<AlertDialog.Trigger asChild let:builder>
+					<Button builders={[builder]} on:click={handleLogin} disabled={loading}>
+						<img
+							src={$mode === 'dark'
+								? 'https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/653714c17467993e7b389c83_636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg'
+								: 'https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/653714c1f22aef3b6921d63d_636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg'}
+							alt="Discord"
+							class="h-5 w-5 mr-2"
+							/>
+						{loading ? 'Loading' : 'Login with Discord'}</Button
+					>
+				</AlertDialog.Trigger>
+				<AlertDialog.Content>
+					<AlertDialog.Header>
+						<AlertDialog.Title>You are soon to be redirected.</AlertDialog.Title>
+						<AlertDialog.Description>
+							Once you authorise with Discord, you will be redirected back to Lyntr.com
+						</AlertDialog.Description>
+					</AlertDialog.Header>
+				</AlertDialog.Content>
+			</AlertDialog.Root>
+		</div>
 	</div>
 </div>
