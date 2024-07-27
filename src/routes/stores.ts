4d96847f53b1ab44d5bd887c2f6430c7377b26a9
Linker <66379820+Linker-123@users.noreply.github.com>
2024-07-27 23:11:35 +0300

    Update stores.ts

--- src/routes/stores.ts
+++ src/routes/stores.ts
@@ -3,5 +3,5 @@ import { readable, writable } from 'svelte/store';
 export const currentPage = writable('home');
 export const v = String(Math.random());
 export const cdnUrl = (filename: string, dimension: string | null = null) => {
-	return `http://localhost:9000/lyntr/${filename}${dimension ? '_' + dimension : ''}.webp?v=${v}`;
-};
+    return `https://cdn.lyntr.com/lyntr/${filename}${dimension ? '_' + dimension : ''}.webp?v=${v}`
+}
