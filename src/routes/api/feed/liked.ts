0125d736df2620b1effa9a1503f022d6566f50fc
Face <69168154+face-hh@users.noreply.github.com>
2024-07-29 09:15:44 +0300

    fix(liked): no longer includes lynts liked by others

--- src/routes/api/feed/liked.ts
+++ src/routes/api/feed/liked.ts
@@ -5,24 +5,15 @@ import { lyntObj } from '../util';
 
 export async function likedFeed(userId: string) {
 	const feed = await db
-		.select(lyntObj(userId))
-		.from(lynts)
-		.leftJoin(likes, eq(likes.lynt_id, lynts.id))
-		.leftJoin(users, eq(lynts.user_id, users.id))
-		.where(
-			and(
-				or(isNull(lynts.parent), eq(lynts.reposted, true)),
-				exists(
-					db
-						.select()
-						.from(likes)
-						.where(and(eq(likes.user_id, userId), eq(likes.lynt_id, lynts.id)))
-				)
-			)
-		)
-		//.groupBy(lynts.id, users.id)
-		.orderBy(desc(likes.liked_at))
-		.limit(100);
+		.select({
+			...lyntObj(userId),
+			likedAt: likes.liked_at
+		})
+		.from(likes)
+		.innerJoin(lynts, eq(likes.lynt_id, lynts.id))
+		.innerJoin(users, eq(lynts.user_id, users.id))
+		.where(eq(likes.user_id, userId))
+		.orderBy(desc(likes.liked_at));
 
 	return feed;
 }
