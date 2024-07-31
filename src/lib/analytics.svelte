e871f564de49125a6693e0a46b72004e4c6d16a2
Face <69168154+face-hh@users.noreply.github.com>
2024-07-31 09:33:53 +0300

    testing google analytics (3)

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-DCLJJ37WDZ', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-DCLJJ37WDZ">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-DCLJJ37WDZ');
+	</script>
+</svelte:head>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Analytics from '$lib/analytics.svelte';
+</script>
+
+<Analytics />
+
+<slot />
