f4241b212f9a19bd0bf8dab69894635d6853565e
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2024-08-01 03:16:14 +0200

    Prevent user from posting empty lynt

--- src/routes/DivInput.svelte
+++ src/routes/DivInput.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
 	export let lynt: string = '';
 
 	$: characterCount = lynt.length;
 	$: isOverLimit = characterCount > 280;
+
+	const dispatch = createEventDispatcher<{
+        input: Event;
+    }>();
 
 	function handlePaste(event: ClipboardEvent) {
 		event.preventDefault();
 		const text = event.clipboardData?.getData('text/plain') || '';
 		document.execCommand('insertText', false, text);
 	}
-
+	
 	function interfere(event: KeyboardEvent) {
+		
 		if (event.key === 'Enter') {
 			event.preventDefault();
 			document.execCommand('insertText', false, '\n');
 		}
+	}
+
+	function handleInput(event: Event) {
+		dispatch('input', event);
 	}
 </script>
 
@@ -29,6 +40,7 @@
 		placeholder="What's happening?"
 		on:paste={handlePaste}
 		on:keydown={interfere}
+		on:input={handleInput}
 	/>
 	<div class="absolute bottom-1 right-1 rounded px-1 text-sm" class:text-red-500={isOverLimit}>
 		{characterCount}/280
