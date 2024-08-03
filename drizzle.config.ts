b3c62e7ede0313c2b69b35a8916734f424f34db6
GStudiosX <76548041+GStudiosX2@users.noreply.github.com>
2024-08-03 23:18:18 +0100

    Use DATABASE_URL for drizzle instead of DB_HOST etc

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,19 +3,12 @@ import type { Config } from 'drizzle-kit';
 
 config({ path: '.env' })
 
-if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
-  throw new Error('Missing database credentials in environment variables');
-}
-
 export default {
   schema: './src/lib/server/schema.ts',
   out: './drizzle',
   dialect: 'postgresql', // 'postgresql' | 'mysql' | 'sqlite'
   dbCredentials: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
     ssl: false
   },
 } satisfies Config;
