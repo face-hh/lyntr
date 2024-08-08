f800e3ffb2cc094761a837a286322ecb50e95841
GStudiosX <76548041+GStudiosX2@users.noreply.github.com>
2024-08-08 16:29:15 +0100

    Add type:users filter

--- src/routes/Search.svelte
+++ src/routes/Search.svelte
@@ -2,13 +2,20 @@
 	import { Label } from '$lib/components/ui/label';
 	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button';
+	import { Badge } from '$lib/components/ui/badge';
 	import Lynt from './Lynt.svelte';
 	import { onMount } from 'svelte';
 	import LoadingSpinner from './LoadingSpinner.svelte';
 	import { toast } from 'svelte-sonner';
+	import Avatar from './Avatar.svelte';
+	import { cdnUrl, currentPage } from './stores';
+	import { goto } from '$app/navigation';
 
 	let searchQuery = '';
-	let searchResults: any[] = [];
+	let searchResults: {
+		results: any[];
+		type: 'lynts' | 'users';
+	} = { results: [], type: 'lynts' };
 	let isLoading = false;
 	let hasSearched = false;
 
@@ -49,12 +56,19 @@
 	}
 </script>
 
-<div class="mx-auto flex h-full w-full flex-col items-center mt-5 {small ? "max-w-[400px]" : ""} md:max-w-[600px]">
-	<h1 class="mb-4 text-2xl font-bold {hideTitle ? 'hidden' : ''}">Search Lynts</h1>
+<div
+	class="mx-auto mt-5 flex h-full w-full flex-col items-center {small
+		? 'max-w-[400px]'
+		: ''} md:max-w-[600px]"
+>
+	<h1 class="mb-4 text-2xl font-bold {hideTitle ? 'hidden' : ''}">
+		Search {searchResults.type?.charAt(0)?.toUpperCase() +
+			searchResults.type?.slice(1)?.toLowerCase()}
+	</h1>
 	<div class="mb-4 flex w-full p-1">
 		<Input
 			type="text"
-			placeholder="Search for lynts..."
+			placeholder="Search for {searchResults.type}..."
 			bind:value={searchQuery}
 			on:keypress={handleKeyPress}
 			on:input={handleInput}
@@ -67,10 +81,32 @@
 			{#if isLoading}
 				<LoadingSpinner />
 			{:else if hasSearched}
-				{#if searchResults.length > 0}
+				{#if searchResults.results.length > 0}
 					<div class="flex flex-col gap-4 px-1">
-						{#each searchResults as lynt}
-							<Lynt {...lynt} myId={userId} lyntClick={handleLyntClick} {small} />
+						{#each searchResults.results as result}
+							{#if searchResults.type === 'lynts'}
+								<Lynt {...result} myId={userId} lyntClick={handleLyntClick} {small} />
+							{:else}
+								<div
+									class="flex flex-row gap-2 rounded-xl bg-secondary/70 p-3"
+									on:click={() => {
+										currentPage.set(`profile${result.handle}`);
+										goto(`/@${result.handle}`);
+									}}
+								>
+									<Avatar src={cdnUrl(result.id, 'medium')} alt={result.username} border={true} />
+									<div class="flex flex-col gap-2">
+										<div class="flex flex-row gap-2">
+											<span class="max-w-64 overflow-hidden truncate">{result.username}</span>
+											<Badge>{result.iq}</Badge>
+											<span class="max-w-64 overflow-hidden truncate text-muted-foreground"
+												>@{result.handle}</span
+											>
+										</div>
+										<span class="break-word w-full overflow-hidden">{result.bio}</span>
+									</div>
+								</div>
+							{/if}
 						{/each}
 					</div>
 				{:else}
--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import type { RequestHandler } from '@sveltejs/kit';
 import { db } from '@/server/db';
 import { lynts, users, likes, followers } from '@/server/schema';
-import { sql, and, eq, ilike, desc } from 'drizzle-orm';
+import { sql, and, or, eq, ilike, desc } from 'drizzle-orm';
 import { verifyAuthJWT } from '@/server/jwt';
 import { lyntObj } from '../util';
 
@@ -29,32 +29,64 @@ export const GET: RequestHandler = async ({ url, cookies }) => {
 
 		let cleanedQuery = query
 			.replace(/from:@([^ ]+)/g, '')
+			.replace(/type:([^ ]+)/g, '')
 			.trim();
-		let whereClause = ilike(lynts.content, `%${cleanedQuery}%`);
 
-		const match = query.match(/from:@([^ ]+)/);
-		if (match) {
-			whereClause = and(
-				eq(users.handle, match[1].replace(/^@/, '')),
-				whereClause
+		let results: any[] = [];
+                const typeMatch = query.match(/type:([^ ]+)/);
+
+		if (typeMatch && typeMatch[1] === 'users') {
+			let whereClause = or(
+				ilike(users.username, `%${cleanedQuery}%`),
+				ilike(users.handle, `%${cleanedQuery}%`),
 			);
+
+
+			results = await db
+				.select({
+					id: users.id,
+					username: users.username,
+					handle: users.handle,
+					bio: users.bio,
+					iq: users.iq,
+					verified: users.verified,
+					created_at: users.created_at,
+				})
+				.from(users)
+				.where(whereClause)
+				.orderBy(desc(users.iq))
+				.limit(50)
+				.execute();
+                } else {
+			let whereClause = ilike(lynts.content, `%${cleanedQuery}%`);
+
+			const match = query.match(/from:@([^ ]+)/);
+			if (match) {
+				whereClause = and(
+					eq(users.handle, match[1].replace(/^@/, '')),
+					whereClause
+				);
+			}
+
+			results = await db
+				.select(lyntObj(userId))
+				.from(lynts)
+				.leftJoin(likes, eq(likes.lynt_id, lynts.id))
+				.leftJoin(users, eq(lynts.user_id, users.id))
+				.where(whereClause)
+				.groupBy(lynts.id, users.id)
+				.orderBy(desc(lynts.created_at))
+				.limit(50)
+				.execute();
+
+			// Increment view counts in the background
+			incrementViewCounts(results.map((result) => result.id));
 		}
 
-		const searchResults = await db
-			.select(lyntObj(userId))
-			.from(lynts)
-			.leftJoin(likes, eq(likes.lynt_id, lynts.id))
-			.leftJoin(users, eq(lynts.user_id, users.id))
-			.where(whereClause)
-			.groupBy(lynts.id, users.id)
-			.orderBy(desc(lynts.created_at))
-			.limit(50)
-			.execute();
-
-		// Increment view counts in the background
-		incrementViewCounts(searchResults.map((result) => result.id));
-
-		return json(searchResults, { status: 200 });
+		return json({
+			results,
+			type: typeMatch && typeMatch[1] === 'users' ? 'users' : 'lynts'
+		}, { status: 200 });
 	} catch (error) {
 		console.error('Error performing search:', error);
 		return json({ error: 'Failed to perform search' }, { status: 500 });
