190feb5a17e82f5d3c2563baf8f62564a3985d02
GStudiosX <76548041+GStudiosX2@users.noreply.github.com>
2024-08-09 14:02:15 +0100

    remove src/routes/+page.ts

--- src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({ setHeaders, parent }) => {
-	// Cache for 1 day
-	setHeaders({
-		'cache-control': 'Cache-control: max-age=86400, public',
-	});
-	return parent();
-};
