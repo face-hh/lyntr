ae5f87189c685633a078fa43afec26765615205e
Face <69168154+face-hh@users.noreply.github.com>
2024-08-08 23:41:54 -0700

    asynchronously remove loading for bottom feed

--- src/routes/MainPage.svelte
+++ src/routes/MainPage.svelte
@@ -105,9 +105,9 @@
 			if (scrollTop + clientHeight >= scrollHeight - 5 && !loadingBottomFeed) {
 				loadingBottomFeed = true;
 
-				fetchFeed(true);
-
-				loadingBottomFeed = false;
+				fetchFeed(true).then(() => {
+					loadingBottomFeed = false;
+				});
 			}
 		}
 	}
@@ -230,7 +230,7 @@
 	});
 
 	async function renderLyntAtTop(lyntId: string) {
-		const lynt = await getLynt(lyntId)
+		const lynt = await getLynt(lyntId);
 		feed = [lynt].concat(feed);
 	}
 	function handlePaste(event: ClipboardEvent) {
